9974f220acc810b39e28ba0b664624122e314296
Tadas Verbyla <90239993+TadasVerbyla@users.noreply.github.com>
2024-12-18 13:42:43 +0200

    Aggregation service
    Implemented service layer functionality that downloads csv statistics and aggregates them

---
+++ PowerAggregator/Models/PowerUsageRecord.cs
@@ -0,0 +1,24 @@
+﻿namespace PowerAggregator.Models
+{
+    public struct PowerUsageRecord
+    {
+        public string RegionName { get; }
+        public string ObjectName { get; }
+        public string ObjectType { get; }
+        public int Number { get; }
+        public decimal ConsumedPower { get; }
+        public DateTime Date { get; }
+        public decimal ProducedPower { get; }
+
+        public PowerUsageRecord(string regionName, string objectName, string objectType, int number, decimal consumedPower, DateTime date, decimal producedPower)
+        {
+            RegionName = regionName;
+            ObjectName = objectName;
+            ObjectType = objectType;
+            Number = number;
+            ConsumedPower = consumedPower;
+            Date = date;
+            ProducedPower = producedPower;
+        }
+    }
+}
---
+++ PowerAggregator/Services/AggregationService.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using PowerAggregator.Models;
+using System.Diagnostics;
+using System.Net;
+
+namespace PowerAggregator.Services
+{
+    public class AggregationService
+    {
+        public void ProcessStatisticUrl(string url)
+        {
+            string csv = DownloadCSV(url).Result;
+            var records = ParseCSV(csv);
+            var apartments = FilterByObjectName(records, "BUTAS");
+            var statistics = AggregateRegionStatistics(apartments);
+        }
+
+        private List<MonthlyRegionStatistic> AggregateRegionStatistics(List<PowerUsageRecord> apartmentRecords)
+        {
+            return apartmentRecords
+                .GroupBy(record => new { record.RegionName, YearMonth = new DateTime(record.Date.Year, record.Date.Month, 1) })
+                .Select(statistic => new MonthlyRegionStatistic
+                {
+                    RegionName = statistic.Key.RegionName,
+                    MonthDate = statistic.Key.YearMonth,
+                    ConsumedPower = statistic.Sum(record => record.ConsumedPower),
+                    ProducedPower = statistic.Sum(record => record.ProducedPower),
+                })
+                .ToList();
+        }
+
+        private List<PowerUsageRecord> FilterByObjectName(List<PowerUsageRecord> records, string objectName)
+        {
+            return records.Where(record => record.ObjectName == objectName).ToList();
+        }
+
+        private List<PowerUsageRecord> ParseCSV(string csv)
+        {
+            var rows = csv.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            var records = new List<PowerUsageRecord>();
+
+            foreach (var row in rows.Skip(1))
+            {
+                var elements = row.Split(';');
+
+                var regionName = elements[0];
+                var objectName = elements[1];
+                var objectType = elements[2];
+                var number = int.Parse(elements[3]);
+                var powerConsumed = decimal.Parse(elements[4]);
+                var date = DateTime.Parse(elements[5]);
+                var powerProduced = 0m;
+                if (!string.IsNullOrEmpty(elements[6]))
+                {
+                    powerProduced = decimal.Parse(elements[6]);
+                }
+
+                var record = new PowerUsageRecord(
+                    regionName,
+                    objectName,
+                    objectType,
+                    number,
+                    powerConsumed,
+                    date,
+                    powerProduced
+                );
+                records.Add(record);
+            }
+            return records;
+        }
+
+        private async Task<string> DownloadCSV(string url)
+        {
+            using HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+            throw new Exception($"Failed to download {url}");  
+        }
+    }
+}
